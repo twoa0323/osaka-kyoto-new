44d399819458e604f8030b3540c99ad536ff671b
黃晨瑋 <huangchenwei@MacBook-Pro.local>
2026-02-24 21:02:21 +0800

    Update osaka-kyoto-new

--- vite.config.js
+++ vite.config.js
@@ -78,13 +78,5 @@ export default defineConfig({
         ]
       }
     })
-  ],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-      }
-    }
-  }
+  ]
 })
